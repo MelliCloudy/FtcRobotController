229d03dddcb2faacd6abe09ff54bb19f4204a72b
MelliCloudy <29eyan@stratfordschools.net>
2025-09-08 17:28:47 -0700

    test motor gamepad first attempt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 //Type of Program (Auto or TeleOp)
@@ -14,9 +15,24 @@ public class MotorTest extends LinearOpMode {
         motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         waitForStart();
 
-        while (opModeIsActive()){
+        while (opModeIsActive()) {
+            //previousGamepad1.copy(currentGamepad1);
+            //previousGamepad2.copy(currentGamepad2);
+            //currentGamepad1.copy(gamepad1);
+            //currentGamepad2.copy(gamepad2);
+            if (gamepad1.a) motor.setPower(1);
+            else motor.setPower(0);
+        }
+        /*
+        DcMotor motor = hardwareMap.get(DcMotor.class, "test");
+        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        waitForStart();
+
+        while (opModeIsActive() && gamepad1.a){
             motor.setPower(0.5);
         }
         motor.setPower(0);
+        */
     }
 }
