4bb2daa6f7d72a65b9f84a404a447c0ed90ff7ab
reyakashyap <kashyapreya@gmail.com>
2025-09-15 15:47:44 -0700

    motor test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
@@ -54,8 +54,8 @@ public class MotorTest extends LinearOpMode {
     }
 
     public void frontForward(double power, DcMotor left, DcMotor right) throws InterruptedException {
-        left.setPower(-power*1.5);
-        right.setPower(power*1.5);
+        left.setPower(-power*10);
+        right.setPower(power*10);
     }
     public void frontHorizontal(double power, DcMotor left, DcMotor right) throws InterruptedException {
         left.setPower(power);
